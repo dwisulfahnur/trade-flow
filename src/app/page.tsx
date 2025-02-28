8ef37ec1a36596b60d465f91131f553bc7f87e45
Dwi Sulfahnur <dwisulfahnur@gmail.com>
2025-02-28 14:31:47 +0700

    feat: set function page name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 
 import { Center, Text } from "@chakra-ui/react";
 
-export default function () {
+export default function IndexPage() {
   return (
     <Center minH='100vh' w='full'>
       <Text>Trade Flow</Text>
