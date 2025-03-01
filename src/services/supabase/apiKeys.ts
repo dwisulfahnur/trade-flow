15e4b787a06202cb525944443aaf93439a4b2ad9
Dwi Sulfahnur <dwisulfahnur@gmail.com>
2025-03-01 20:18:09 +0700

    feat: add services

---
+++ src/services/supabase/apiKeys.ts
@@ -0,0 +1,71 @@
+import { SupabaseClient } from "@supabase/supabase-js";
+import { UseSessionReturn } from '@clerk/types'
+import createClerkSupabaseClient from "@/lib/supabase";
+import { Database } from "@/types/database.types";
+
+class ApiKeysService {
+  private supabaseClient: SupabaseClient;
+
+  constructor(private session: UseSessionReturn['session']) {
+    this.supabaseClient = createClerkSupabaseClient(session);
+
+    this.getApiKeys = this.getApiKeys.bind(this);
+    this.createApiKey = this.createApiKey.bind(this);
+    this.updateApiKey = this.updateApiKey.bind(this);
+    this.deleteApiKey = this.deleteApiKey.bind(this);
+  }
+
+  async getApiKeys(): Promise<Database['public']['Tables']['api_keys']['Row'][]> {
+    const { data, error } = await this.supabaseClient.from('api_keys').select('*');
+    if (error) throw error;
+    return data
+  }
+
+  async createApiKey({ apiKey, secretKey, exchange }: {
+    apiKey: string,
+    secretKey: string,
+    exchange: string
+  }): Promise<Database['public']['Tables']['api_keys']['Row']> {
+    const { data, error } = await this.supabaseClient.from('api_keys').insert({
+      exchange,
+      api_key: apiKey,
+      api_secret: secretKey,
+    }).select().single();
+
+    if (error) throw error;
+    return data;
+  }
+
+  async updateApiKey({ id, apiKey, secretKey, exchange }: {
+    id: string,
+    apiKey: string,
+    secretKey: string,
+    exchange: string
+  }): Promise<Database['public']['Tables']['api_keys']['Row']> {
+    const { data, error } = await this.supabaseClient.from('api_keys')
+      .update({
+        api_key: apiKey,
+        api_secret: secretKey,
+        exchange
+      })
+      .eq('id', id)
+      .select()
+      .single();
+
+    if (error) throw error;
+    return data;
+  }
+
+  async deleteApiKey(id: string) {
+    const { data, error } = await this.supabaseClient.from('api_keys')
+      .delete()
+      .eq('id', id)
+      .select()
+      .single();
+
+    if (error) throw error;
+    return data;
+  }
+}
+
+export default ApiKeysService;
